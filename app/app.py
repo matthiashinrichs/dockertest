ac94a92bd7af9a1f89bf5be14c80b26defd5bb43
Matthias Hinrichs <matthias.hinrichs@me.com>
2019-01-28 23:49:01 +0100

    added some style

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ def hello_whale():
     os_env = os.environ
     r = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     hostname = socket.gethostname()
-    return "Whale, Hello there! <br>%s<br><br>%s<br><br>%s"%(hostname, os_env, r)
+    return "<h2>Hello there!</h2> <br><br>%s<br><br>%s<br><br>%s"%(hostname, os_env, r)
 
 
 if __name__ == '__main__':
