c11996c4212d6235bc4f20437fd54ee955250889
Matthias Hinrichs <matthias.hinrichs@me.com>
2019-01-28 23:12:40 +0100

    implemented more parameters to show

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 from flask import Flask
-import socket 
+from flask import request
+import socket
+import os
 
 app = Flask(__name__)
  
  
 @app.route('/')
 def hello_whale():
+    os_env = os.environ
+    r = request.remote_addr
     hostname = socket.gethostname()
-    return "Whale, Hello there! %s"%(hostname)
- 
+    return "Whale, Hello there! <br>%s<br><br>%s<br><br>%s"%(hostname, os_env, r)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
