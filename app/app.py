00f3f1c97c70248e0cb4670ba89de5aa2ea87d1c
Matthias Hinrichs <matthias.hinrichs@me.com>
2019-01-28 23:58:14 +0100

    added some style and finally got client ip shown

--- app/app.py
+++ app/app.py
@@ -9,9 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_whale():
     os_env = os.environ
-    r = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    r_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    req_env = request.environ
     hostname = socket.gethostname()
-    return "<h2>Hello there!</h2> <br><br>%s<br><br>%s<br><br>%s"%(hostname, os_env, r)
+    return "<h2>Hello there!</h2> <br><br><b>hostname/container-id:</b> %s<br><br>OS Environment Vars:<br>%s<br><br>Request Environment Vars:<br>%s<br><br>Client IP: %s"%(hostname, os_env, req_env, r_ip)
 
 
 if __name__ == '__main__':
