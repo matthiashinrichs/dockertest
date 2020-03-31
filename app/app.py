0b1158b12f96ef47dc31622188347d8484752326
Matthias <matthias.hinrichs@me.com>
2020-03-31 21:43:19 +0200

    add build_number to html output

--- app/app.py
+++ app/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_index():
     os_env = os.environ
+    build_number = os.environ.get('BUILD_NUMBER', '0')
     python_version = sys.version
     r_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     x_forwarded_ip = request.environ.get('HTTP_X_ORIGINAL_FORWARDED_FOR', '0')
@@ -24,6 +25,7 @@ def hello_index():
         'request_env': req_env,
         'x_forwarded_ip': x_forwarded_ip,
         'hostname': hostname,
+        'build_number': build_number,
         'css_version': css_version
     }
     return render_template('index.html', title='Home', data=data)
