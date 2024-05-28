5cf6b5b37f32f40c57388416ef1d6c24760eedff
Matthias Hinrichs <matthias.hinrichs@sbb.ch>
2024-05-28 17:21:27 +0200

    Updated Requirements.txt and changed port

--- app/app.py
+++ app/app.py
@@ -6,8 +6,8 @@ import sys
 import os
 
 app = Flask(__name__)
- 
- 
+
+
 @app.route('/')
 def hello_index():
     os_env = os.environ
@@ -32,4 +32,4 @@ def hello_index():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port='8080')
